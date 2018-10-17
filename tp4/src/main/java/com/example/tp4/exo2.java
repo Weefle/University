c1c98b54a296a4ee09754c367fd278fc1d21c594
Weefle <mr.weefle@gmail.com>
2018-10-17 16:17:34 +0200

    new exercices

--- tp4/src/main/java/com/example/tp4/exo2.java
+++ tp4/src/main/java/com/example/tp4/exo2.java
@@ -1,7 +1,32 @@
 package com.example.tp4;
 
+import java.util.Scanner;
+
 public class exo2 {
 
+    // condition: il faut que d>0
+    public static String decimal2hex(int d) {
+        String digits = "0123456789ABCDEF";
+        if (d <= 0) return "0";
+        int base = 16;   // flexible to change in any base under 16
+        String hex = "";
+        while (d > 0) {
+            int digit = d % base;              // rightmost digit
+            hex = digits.charAt(digit) + hex;  // string concatenation
+            d = d / base;
+        }
+        return hex;
+    }
 
+    public static void main(String[] args){
+        int d;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Entrer un nombre entier sur 32 bits : ");
+            d = sc.nextInt();
+        }while(d==0);
+        System.out.print(decimal2hex(d));
+
+    }
 
 }
---
+++ tp4/src/main/java/com/example/tp4/exo3.java
@@ -0,0 +1,11 @@
+package com.example.tp4;
+
+public class exo3 {
+
+    public static void main(String[] args){
+
+
+
+    }
+
+}
